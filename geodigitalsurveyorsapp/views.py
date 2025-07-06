4f66df48c3e319bfb96a2eca5597133e1baacc86
bmcouma <mireribravin2019@gmail.com>
2025-07-06 15:32:23 +0300

    Added routing and static pages: About, Services, Gallery, Contact

--- geodigitalsurveyorsapp/views.py
+++ geodigitalsurveyorsapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contact(request):
+    return render(request, 'contact.html')
